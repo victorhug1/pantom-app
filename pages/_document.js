e3a457a489e91cd669e8810d2bbea106adacd8e1
“victor <victorhug1@gmail.com>
2025-05-15 13:50:38 -0500

    feat: agregar Google Tag Manager y Hotjar para analítica global

--- pages/_document.js
+++ pages/_document.js
@@ -7,8 +7,36 @@ export default function Document() {
         <link rel="icon" href="/pantom_logo.svg" />
         <link rel="apple-touch-icon" href="/pantom_logo.svg" />
         <meta name="theme-color" content="#000000" />
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MN7GS3TZ');
+        `}} />
+        {/* Hotjar Tracking Code */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:6405051,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        `}} />
       </Head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MN7GS3TZ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
